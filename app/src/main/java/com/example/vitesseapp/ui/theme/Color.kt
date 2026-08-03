3560fac29c7a7b4fd73d483cda4a806f26218766
Gaëlle Bossard <bossard.gaelle@gmail.com>
2026-08-03 16:44:59 +0200

    feat: implement new colors for the theme of the app

--- app/src/main/java/com/example/vitesseapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/vitesseapp/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.example.vitesseapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val colorPrimary = Color(0xFFDF9780)
+val colorPrimaryVariant = Color(0xFF749FB8)
+val colorOnPrimary = Color(0xFFFFFFFF)
+val colorSecondary = Color(0xFFE8B952)
+val colorSecondaryVariant = Color(0xFFE8B952)
+val colorOnSecondary = Color(0xFFFFFFFF)
