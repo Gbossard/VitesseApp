1ec8daf4dbead8b917afa5a9943cf733cee09d2c
Gaëlle Bossard <bossard.gaelle@gmail.com>
2026-08-04 14:56:35 +0200

    feat(home): create primary tab for all candidates

--- app/src/main/java/com/example/vitesseapp/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/vitesseapp/ui/screens/home/HomeScreen.kt
@@ -1,13 +1,63 @@
 package com.example.vitesseapp.ui.screens.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PrimaryTabRow
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.vitesseapp.R
 import com.example.vitesseapp.ui.theme.VitesseAppTheme
 
 @Composable
 fun HomeScreen() {
-    Text("HomeScreen")
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            // TODO search composable
+            HomeTabs()
+        }
+    }
+}
+
+@Composable
+fun HomeTabs(modifier: Modifier = Modifier) {
+    var state by rememberSaveable { mutableIntStateOf(0) }
+    val titles = listOf(stringResource(R.string.tab_item_all), "Tab 2")
+    Column(
+        modifier = modifier
+    ) {
+        PrimaryTabRow(selectedTabIndex = state) {
+            titles.forEachIndexed { index, title ->
+                Tab(
+                    selected = state == index,
+                    unselectedContentColor = Color.Unspecified,
+                    onClick = { state = index },
+                    text = { Text(text = title, maxLines = 2, overflow = TextOverflow.Ellipsis) },
+                )
+            }
+        }
+        if (state == 0 ) {
+            Text(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                text = "All candidates tab",
+                style = MaterialTheme.typography.bodyLarge,
+            )
+        }
+    }
 }
 
 
