05d8d34bcc1adf3143c6534269f5639f66c7510f
Gaëlle Bossard <bossard.gaelle@gmail.com>
2026-08-05 17:10:15 +0200

    feat(data): implement dao query to get all candidates

--- app/src/main/java/com/example/vitesseapp/data/local/CandidateDao.kt
+++ app/src/main/java/com/example/vitesseapp/data/local/CandidateDao.kt
@@ -1,7 +1,11 @@
 package com.example.vitesseapp.data.local
 
 import androidx.room3.Dao
+import androidx.room3.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CandidateDao {
+    @Query("SELECT * FROM candidate_table")
+    fun getAllCandidates(): Flow<List<CandidateEntity>>
 }
