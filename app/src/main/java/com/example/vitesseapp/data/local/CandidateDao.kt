e3c798c93e18f5bcf7cfae8d23e84bda0e717f84
Gaëlle Bossard <bossard.gaelle@gmail.com>
2026-08-18 15:57:24 +0200

    feat(data): implement favorite filter by firstName and lastName in Dao and Repository

--- app/src/main/java/com/example/vitesseapp/data/local/CandidateDao.kt
+++ app/src/main/java/com/example/vitesseapp/data/local/CandidateDao.kt
@@ -13,6 +13,10 @@ interface CandidateDao {
     """)
     fun getAllCandidates(query: String): Flow<List<CandidateEntity>>
 
-    @Query("SELECT * FROM candidate_table WHERE isFavorite = 1")
-    fun getAllFavorites(): Flow<List<CandidateEntity>>
+    @Query(""" 
+        SELECT * FROM candidate_table WHERE isFavorite = 1
+        AND ((firstName || ' ' || lastName) LIKE '%' || :query || '%' 
+            OR (lastName || ' ' || firstName) LIKE '%' || :query || '%' )
+    """)
+    fun getAllFavorites(query: String): Flow<List<CandidateEntity>>
 }
