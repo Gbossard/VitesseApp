7c9f18917d8b863f06853725b569f30f9d9d553f
Gaëlle Bossard <bossard.gaelle@gmail.com>
2026-08-21 10:54:14 +0200

    test(dateConverter): add unit tests for fromString and toString

---
+++ app/src/test/java/com/example/vitesseapp/data/local/DateConverterTest.kt
@@ -0,0 +1,27 @@
+package com.example.vitesseapp.data.local
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import java.time.LocalDate
+
+class DateConverterTest {
+    private lateinit var dateConverter: DateConverter
+    @Before
+    fun setUp() {
+        dateConverter = DateConverter()
+    }
+
+    @Test
+    fun fromString_returnsLocalDate() {
+        val result = dateConverter.fromString("2026-01-01")
+        assertEquals(LocalDate.of(2026,1,1), result)
+    }
+
+    @Test
+    fun toString_returnsString() {
+        val result = dateConverter.toString(LocalDate.of(2026,1,1))
+        assertEquals("2026-01-01", result)
+    }
+
+}
