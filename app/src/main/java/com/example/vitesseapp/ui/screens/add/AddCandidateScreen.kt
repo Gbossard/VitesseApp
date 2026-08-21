8975b839cefe544337ce743950c88cce149bd324
Gaëlle Bossard <bossard.gaelle@gmail.com>
2026-08-21 18:04:59 +0200

    feat(ui): implement first name and last name form

--- app/src/main/java/com/example/vitesseapp/ui/screens/add/AddCandidateScreen.kt
+++ app/src/main/java/com/example/vitesseapp/ui/screens/add/AddCandidateScreen.kt
@@ -1,19 +1,29 @@
 package com.example.vitesseapp.ui.screens.add
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.vitesseapp.R
 import com.example.vitesseapp.ui.theme.VitesseAppTheme
 
@@ -27,8 +37,44 @@ fun AddCandidateScreen(
         },
     ) { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
         ) {
+            NameSection()
+        }
+    }
+}
+
+@Composable
+fun NameSection(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Icon(
+            modifier = Modifier.padding(top = 8.dp, end = 16.dp),
+            painter = painterResource(R.drawable.ic_person_24dp),
+            contentDescription = stringResource(R.string.content_description_person_name)
+        )
+        Column {
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                state = rememberTextFieldState(),
+                lineLimits = TextFieldLineLimits.SingleLine,
+                label = { Text(stringResource(R.string.form_first_name)) }
+            )
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 32.dp),
+                state = rememberTextFieldState(),
+                lineLimits = TextFieldLineLimits.SingleLine,
+                label = { Text(stringResource(R.string.form_last_name)) }
+            )
         }
     }
 }
@@ -64,5 +110,21 @@ private fun AppBarPreview() {
         AppBar(
             onBackClick = {}
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AddCandidateScreenPreview() {
+    VitesseAppTheme {
+        AddCandidateScreen {  }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun NameSectionPreview() {
+    VitesseAppTheme {
+        NameSection()
     }
 }
