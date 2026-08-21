8291687a12afcb58c68b875a88361da78b0370b0
Gaëlle Bossard <bossard.gaelle@gmail.com>
2026-08-21 16:17:54 +0200

    feat(ui): implement topAppBar with title and navigation button to AddCandidateScreen

--- app/src/main/java/com/example/vitesseapp/ui/screens/add/AddCandidateScreen.kt
+++ app/src/main/java/com/example/vitesseapp/ui/screens/add/AddCandidateScreen.kt
@@ -1,7 +1,68 @@
 package com.example.vitesseapp.ui.screens.add
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.vitesseapp.R
+import com.example.vitesseapp.ui.theme.VitesseAppTheme
 
 @Composable
-fun AddCandidateScreen() {
+fun AddCandidateScreen(
+    onBackClick: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            AppBar(onBackClick = onBackClick)
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+        }
+    }
+}
+
+@Composable
+fun AppBar(
+    modifier: Modifier = Modifier,
+    onBackClick: () -> Unit
+) {
+    TopAppBar(
+        modifier = modifier,
+        title = {
+            Text(text = stringResource(R.string.add_candidate))
+        },
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_arrow_back_24dp),
+                    contentDescription = stringResource(R.string.content_description_back)
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        )
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AppBarPreview() {
+    VitesseAppTheme {
+        AppBar(
+            onBackClick = {}
+        )
+    }
 }
