7e103dedf73851a82027d8bc10e6be1d2f2da90f
Gaëlle Bossard <bossard.gaelle@gmail.com>
2026-08-27 15:24:32 +0200

    test(repository): use MockK instead of fake Dao

--- app/src/test/java/com/example/vitesseapp/data/repository/CandidateRepositoryImplTest.kt
+++ app/src/test/java/com/example/vitesseapp/data/repository/CandidateRepositoryImplTest.kt
@@ -1,29 +1,104 @@
 package com.example.vitesseapp.data.repository
 
-import com.example.vitesseapp.data.local.FakeCandidateDao
+import com.example.vitesseapp.data.local.CandidateDao
+import com.example.vitesseapp.data.local.CandidateEntity
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit4.MockKRule
 import kotlinx.coroutines.flow.first
-import org.junit.Assert.*
-import org.junit.Before
-import org.junit.Test
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.time.LocalDate
 
 class CandidateRepositoryImplTest {
-    private lateinit var candidateDao: FakeCandidateDao
+    @get:Rule
+    val mockkRule = MockKRule(this)
+    @MockK
+    lateinit var candidateDao: CandidateDao
     private lateinit var candidateRepositoryImpl: CandidateRepositoryImpl
 
     @Before
     fun setUp() {
-        candidateDao = FakeCandidateDao()
         candidateRepositoryImpl = CandidateRepositoryImpl(candidateDao)
     }
 
     @Test
     fun getAllCandidates_returnsDataFromDao() = runTest {
-        assertEquals(candidateDao.getAllCandidates("").first(), candidateRepositoryImpl.getAllCandidates("").first())
+        val listOfCandidates = listOf(
+            CandidateEntity(
+                id = "1",
+                firstName = "Fake first name",
+                lastName = "Fake last name",
+                phone = "0606060606",
+                email = "fake.email@fake.com",
+                dateOfBirth = LocalDate.parse("1992-06-20"),
+                photo = null,
+                salary = 0,
+                notes = "fake note",
+                isFavorite = false
+            ),
+            CandidateEntity(
+                id = "2",
+                firstName = "Fake first name 2",
+                lastName = "Fake last name 2",
+                phone = "0606060606",
+                email = "fake2.email@fake.com",
+                dateOfBirth = LocalDate.parse("1999-06-20"),
+                photo = null,
+                salary = 0,
+                notes = "fake note 2",
+                isFavorite = true
+            ),
+            CandidateEntity(
+                id = "3",
+                firstName = "Fake first name 3",
+                lastName = "Fake last name 3",
+                phone = "0606060606",
+                email = "fake3.email@fake.com",
+                dateOfBirth = LocalDate.parse("2000-06-20"),
+                photo = null,
+                salary = 0,
+                notes = "fake note 3",
+                isFavorite = true
+            )
+        )
+        every { candidateDao.getAllCandidates("") } returns flowOf(listOfCandidates)
+        assertEquals(listOfCandidates, candidateRepositoryImpl.getAllCandidates("").first())
     }
 
     @Test
     fun getAllFavorites_returnsDataFromDao() = runTest {
-        assertEquals(candidateDao.getAllFavorites("").first(), candidateRepositoryImpl.getAllFavorites("").first())
+        val listOfFavorites = listOf(
+            CandidateEntity(
+                id = "2",
+                firstName = "Fake first name 2",
+                lastName = "Fake last name 2",
+                phone = "0606060606",
+                email = "fake2.email@fake.com",
+                dateOfBirth = LocalDate.parse("1999-06-20"),
+                photo = null,
+                salary = 0,
+                notes = "fake note 2",
+                isFavorite = true
+            ),
+            CandidateEntity(
+                id = "3",
+                firstName = "Fake first name 3",
+                lastName = "Fake last name 3",
+                phone = "0606060606",
+                email = "fake3.email@fake.com",
+                dateOfBirth = LocalDate.parse("2000-06-20"),
+                photo = null,
+                salary = 0,
+                notes = "fake note 3",
+                isFavorite = true
+            )
+        )
+        every { candidateDao.getAllFavorites("") } returns flowOf(listOfFavorites)
+        assertEquals(listOfFavorites, candidateRepositoryImpl.getAllFavorites("").first())
     }
 }
