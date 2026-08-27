5b7977b6ac7ba9bdbff8cf86e816a30fe542e99f
Gaëlle Bossard <bossard.gaelle@gmail.com>
2026-08-27 15:19:43 +0200

    build: add MockK dependencies for test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
 
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockk.jvm)
 
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
