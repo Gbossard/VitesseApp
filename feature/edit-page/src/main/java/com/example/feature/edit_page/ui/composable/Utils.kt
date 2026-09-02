bbd7c7b96c0548a32072de52b28f50b922c20ec6
Gaëlle Bossard <bossard.gaelle@gmail.com>
2026-09-02 14:50:06 +0200

    feat(feature-edit-page): implement candidate save logic in viewModel and UI

--- feature/edit-page/src/main/java/com/example/feature/edit_page/ui/composable/Utils.kt
+++ feature/edit-page/src/main/java/com/example/feature/edit_page/ui/composable/Utils.kt
@@ -2,8 +2,21 @@ package com.example.feature.edit_page.ui.composable
 
 import android.os.Build
 import android.os.ext.SdkExtensions
+import androidx.annotation.RequiresApi
+import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
 
 fun isEmbeddedPhotoPickerSupported(): Boolean {
     return Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE &&
         SdkExtensions.getExtensionVersion(Build.VERSION_CODES.UPSIDE_DOWN_CAKE) >= 15
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun Long.toLocalDate(): LocalDate {
+    return LocalDateTime.ofInstant(
+        Instant.ofEpochMilli(this),
+        ZoneId.systemDefault()
+    ).toLocalDate()
 }
