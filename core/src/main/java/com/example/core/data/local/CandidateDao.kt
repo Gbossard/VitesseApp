b670a26ddedf10a09965de95f6eebf825edc22b4
Gaëlle Bossard <bossard.gaelle@gmail.com>
2026-09-02 14:44:58 +0200

    feat(core): implement upsert function to edit candidate

--- core/src/main/java/com/example/core/data/local/CandidateDao.kt
+++ core/src/main/java/com/example/core/data/local/CandidateDao.kt
@@ -2,6 +2,7 @@ package com.example.core.data.local
 
 import androidx.room3.Dao
 import androidx.room3.Query
+import androidx.room3.Upsert
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -19,4 +20,7 @@ interface CandidateDao {
             OR (lastName || ' ' || firstName) LIKE '%' || :query || '%' )
     """)
     fun getAllFavorites(query: String): Flow<List<CandidateEntity>>
+
+    @Upsert
+    suspend fun upsertCandidate(candidate: CandidateEntity)
 }
