a63e40898e973f0fcdfda9479ae6ea71889ef95f
Gaëlle Bossard <bossard.gaelle@gmail.com>
2026-09-17 12:12:13 +0200

    feat(feature-edit-page): implement delete old photo in storage when candidate save a new photo

--- core/src/main/java/com/example/core/data/storage/PhotoStorage.kt
+++ core/src/main/java/com/example/core/data/storage/PhotoStorage.kt
@@ -2,11 +2,14 @@ package com.example.core.data.storage
 
 import android.content.Context
 import android.net.Uri
+import androidx.core.net.toFile
+import androidx.core.net.toUri
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.FileNotFoundException
+import java.io.IOException
 import java.util.UUID
 import javax.inject.Inject
 
@@ -28,6 +31,31 @@ class PhotoStorage @Inject constructor(
             val savedFile = File(context.filesDir, fileName)
             val localUri = Uri.fromFile(savedFile).toString()
             Result.success(localUri)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    suspend fun deletePhoto(photo: String?): Result<Unit> = withContext(Dispatchers.IO) {
+        try {
+            if (photo.isNullOrEmpty()) {
+                return@withContext Result.success(Unit)
+            }
+
+            val uri = photo.toUri()
+            val file = uri.toFile()
+
+            if (!file.exists()) {
+                return@withContext Result.success(Unit)
+            }
+
+            val deleted = file.delete()
+            if (deleted) {
+                Result.success(Unit)
+            } else {
+                Result.failure(IOException())
+            }
+
         } catch (e: Exception) {
             Result.failure(e)
         }
